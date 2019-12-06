b62bf24d28fff2e2b3c41888dcd4e73eee425db4
Alexandr Ignatushin <alexandrignatushin@gmail.com>
2019-12-06 20:02:37 +0300

    added link

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -11,7 +11,7 @@
                         <li><a class="f_facebook" href="#"><i class="fab fa-facebook"></i></a></li>
                         <li><a class="f_twitter" href="#"><i class="fab fa-twitter"></i></a></li>
                         <li><a class="f_instagram" href="https://www.instagram.com/odysseus_it/?igshid=1lgijsm9h8zb7" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                        <li><a class="f_linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>
+                        <li><a class="f_linkedin" href="https://www.linkedin.com/company/odysseus-it/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
                         <li><a class="f_youtube" href="#"><i class="fab fa-youtube"></i></a></li>
                         <li><a class="f_upwork" href="https://www.upwork.com/agencies/~01b44c194fa8ee8a53" target="_blank"><img src="{{asset('assets/img/bg/upwork.svg')}}" alt="#"></a></li>
                         <li><a class="f_github" href="https://github.com/odysseus-it" target="_blank"><i class="fab fa-github"></i></a></li>
