088914e8b0be218e3b2a4e4e9828889c5c9d20e7
Alexandr Ignatushin <alexandrignatushin@gmail.com>
2019-12-16 23:53:45 +0300

    linked logo

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-12 col-sm-12 text-center wow zoomIn">
                 <div class="footer_logo">
-                    <a href="index.html"><img src="assets/img/logo.png" alt=""/></a>
+                    <a href="/"><img src="assets/img/logo.png" alt=""/></a>
                 </div>
                 <div class="footer_social">
                     <ul>
